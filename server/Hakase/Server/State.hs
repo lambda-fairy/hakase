309fe5ced74fe25fa817f2668f5d648a236ff180
Chris Wong <lambda.fairy@gmail.com>
2016-12-12 21:41:31 +1300

    Add persistence layer :D

---
+++ server/Hakase/Server/State.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE TemplateHaskell, TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Hakase.Server.State where
+
+import Control.Monad.Reader
+import Control.Monad.State
+import Data.Acid
+import Data.SafeCopy
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IntMap
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Text (Text)
+
+import Hakase.Common
+
+
+-- Orphan instance!! >_<
+deriveSafeCopy 0 'base ''Move
+
+
+data Battle = Battle
+    { battleWhite :: !Text
+    , battleBlack :: !Text
+    , battleMoves :: ![(Move, Move)]
+    } deriving Show
+
+deriveSafeCopy 0 'base ''Battle
+
+
+data ServerState = ServerState
+    { statePlayers :: !(Set Text)
+        -- ^ The set of registered players.
+    , stateBattles :: !(IntMap Battle)
+        -- ^ Maps every battle ID to a corresponding battle.
+    } deriving Show
+
+deriveSafeCopy 0 'base ''ServerState
+
+
+-- | Try to register a player with the given name.
+--
+-- Returns True on success, or False if the name is already taken.
+registerPlayer :: Text -> Update ServerState Bool
+registerPlayer name = state $ \s ->
+    if Set.member name (statePlayers s)
+        then (False, s)
+        else (True, s { statePlayers = Set.insert name (statePlayers s) })
+
+-- | Check if a player has been registered.
+checkPlayer :: Text -> Query ServerState Bool
+checkPlayer name = Set.member name . statePlayers <$> ask
+
+-- | Record the outcome of an epic battle.
+recordBattle :: Battle -> Update ServerState ()
+recordBattle battle = modify $ \s ->
+    let battleId
+            | IntMap.null (stateBattles s) = 0
+            | otherwise = 1 + fst (IntMap.findMax (stateBattles s))
+    in  s { stateBattles = IntMap.insert battleId battle (stateBattles s) }
+
+-- | Look up information on a single battle.
+queryBattle :: Int -> Query ServerState (Maybe Battle)
+queryBattle battleId = IntMap.lookup battleId . stateBattles <$> ask
+
+-- | Look up all battles fought by a particular player.
+playerBattles :: Text -> Query ServerState [(Int, Battle)]
+playerBattles name = IntMap.toList
+    . IntMap.filter (\b -> battleWhite b == name || battleBlack b == name)
+    . stateBattles <$> ask
+
+makeAcidic ''ServerState
+    [ 'registerPlayer
+    , 'checkPlayer
+    , 'recordBattle
+    , 'queryBattle
+    , 'playerBattles
+    ]
